094fd86f317cf3af0097ee9f0f939700f0e7018b
Burak Kaygusuz <kaygusuzburak@gmail.com>
2020-08-19 20:54:10 +0300

    API.Tests project added

---
+++ MovieDatabase.API.Tests/InMemoryWebApplicationFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MovieDatabase.API.Models.Extensions;
+
+namespace MovieDatabase.API.Tests
+{
+    public class InMemoryWebApplicationFactory<T> : WebApplicationFactory<T> where T : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Testing").ConfigureTestServices(services =>
+            {
+                services.AddDbContext<MovieDatabaseContext>(options => options.UseInMemoryDatabase("InMemoryDbForTesting"));
+
+                var serviceProvider = services.BuildServiceProvider();
+                using var scope = serviceProvider.CreateScope();
+                var dbContext = scope.ServiceProvider.GetRequiredService<MovieDatabaseContext>();
+                dbContext.Database.EnsureCreated();
+            });
+        }
+    }
+}
---
+++ MovieDatabase.API.Tests/Tests/MovieControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MovieDatabase.API.Tests.Tests
+{
+    public class MovieControllerTest : IClassFixture<InMemoryWebApplicationFactory<Startup>>
+    {
+        private readonly InMemoryWebApplicationFactory<Startup> factory;
+
+        public MovieControllerTest(InMemoryWebApplicationFactory<Startup> factory)
+        {
+            this.factory = factory;
+        }
+        
+        [Fact]
+        public async Task GetAllMoviesRequest_HttpStatusCode_Returns_OK()
+        {
+            var client = factory.CreateClient();
+            var response = await client.GetAsync("/api/movies");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
